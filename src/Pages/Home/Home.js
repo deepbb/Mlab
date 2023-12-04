f0f75a81da368a6879b1022786b8894bfb272dd8
deepbb <pradeepbb004@gmail.com>
2023-12-04 12:03:56 +0530

    Minor changes in Home Screen

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -3,7 +3,37 @@
 import React from "react";
 import "./Home.css"; // Import CSS file for styling
 import { FaCheck } from "react-icons/fa";
+import { PiMedalBold } from "react-icons/pi";
+import { CiBurger } from "react-icons/ci";
+import { FaStar } from "react-icons/fa6";
+
 function Home() {
+  const HomeBlackBox = ({ backgroundImg, heading, paragraph }) => {
+    const flexStyle = {
+      flex: "1 1 100%",
+      height: "380px",
+      width: "100%",
+      alignItem: "center",
+      justifyContent: "center",
+      paddingTop: "60px",
+      fontSize: "20px",
+      marginRight: "20px",
+      marginBottom: "20px",
+      backgroundImage: `url(${backgroundImg})`,
+      backgroundSize: "cover",
+      opacity: "0.4",
+      color: "#fff",
+      textAlign: "Center",
+    };
+
+    return (
+      <div className="Home-container-black-style" style={flexStyle}>
+        <h2>{heading}</h2>
+        <p>{paragraph}</p>
+      </div>
+    );
+  };
+
   return (
     <div className="Home">
       <div className="Home-container">
@@ -114,12 +144,213 @@ function Home() {
           <h1 className="Home-paragraph-h1">Best Services In Town</h1>
           <div className="Home-paragraph-hr"></div>
           <div className="checklist">
-            <p>Nulla porttitor accumsan tincidunt. Praesent sapien massa, convallis
-             a pellentesque nec, egestas non nisi. Donec sollicitudin molestie.</p>
-             <button className="Home-paragraph2-button">Learn more about us</button>
+            <p>
+              Nulla porttitor accumsan tincidunt. Praesent sapien massa,
+              convallis a pellentesque nec, egestas non nisi. Donec sollicitudin
+              molestie.
+            </p>
+            <button className="Home-paragraph2-button">
+              Learn more about us
+            </button>
           </div>
         </div>
       </div>
+
+      <div className="Home-services-black-background">
+        <h1>Mauris Blandit Aliquet Elit</h1>
+        <div className="Home-services-black-background-underline"></div>
+        <p>
+          Nulla porttitor accumsan tincidunt. Praesent sapien massa, convallis a
+          pellentesque nec, egestas non nisi. Donec sollicitudin molestie.
+        </p>
+        <div className="Home-service-black-container">
+          <div className="Home-service-black">
+            <HomeBlackBox
+              className="Home-service-black1"
+              backgroundImg="assets/Decorating.jpg"
+              heading="Decorating"
+              paragraph="Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua"
+            />
+            <HomeBlackBox
+              backgroundImg="assets/corpate.jpg"
+              heading="Carpentry"
+              paragraph="Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua"
+            />
+          </div>
+          <div className="Home-service-black">
+            <HomeBlackBox
+              backgroundImg="assets/plumbing.jpg"
+              heading="Maintenance"
+              paragraph="Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris."
+            />
+            <HomeBlackBox
+              backgroundImg="assets/maintance.jpg"
+              heading="Plumbing"
+              paragraph="Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore."
+            />
+          </div>
+        </div>
+      </div>
+
+      <div className="Home-help-container">
+        <div className="Home-help-one">
+          <h5 className="Home-help-Header">HIGHLY EXPERIENCED HANDYMEN</h5>
+          <h1 className="Home-Help-h1">We’re Here To Help</h1>
+          <div className="Home-paragraph-hr"></div>
+          <p>
+            Nulla porttitor accumsan tincidunt. Praesent sapien massa, convallis
+            a pellentesque nec, egestas non nisi. Donec sollicitudin molestie.
+          </p>
+          <div className="Home-help-icon-container">
+            <div className="Home-help-h4">
+              <CiBurger className="Home-help-icon" />
+              We’re fully protected with the correct equipment
+            </div>
+            <div className="Home-help-h4">
+              <PiMedalBold className="Home-help-icon" />
+              We have all of the regulatory safety certificates
+            </div>
+          </div>
+        </div>
+        <div className="Home-help-two">
+          <img src="assets/women.jpg" alt="" className="home-help-img"></img>
+        </div>
+      </div>
+
+      <div className="Home-workshop-new-heading">
+        <div className="Home-workshop-heading">
+          <h6>ARTICLES FROM THE WORKSHOP</h6>
+          <h1>Latest Handyman News</h1>
+          <div>
+            <hr className="Home-Workshop-hr"></hr>
+          </div>
+        </div>
+        <div className="Home-workshop-heading1">
+          <button className="Home-workshop-button">Read More News</button>
+        </div>
+      </div>
+      <div className="Home-Workshop-news-container">
+        <div className="Home-Workshop-news">
+          <img
+            src="assets/decking.jpg"
+            alt=""
+            className="Home-workshop-img"
+          ></img>
+          <h3>Staining Your Decking</h3>
+          <h5>282 words 1.6 min read</h5>
+          <div>
+            <hr className="Home-Workshop-hr"></hr>
+          </div>
+          <p className="Home-workshop-paragraph">
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been{" "}
+          </p>
+          <button className="Home-workshop-button">Read More</button>
+        </div>
+        <div className="Home-Workshop-news">
+          <img
+            src="assets/floating.jpg"
+            alt=""
+            className="Home-workshop-img"
+          ></img>
+          <h3>The Best Floating Shelves</h3>
+          <h5>282 words 1.6 min read</h5>
+          <div>
+            <hr className="Home-Workshop-hr"></hr>
+          </div>
+          <p className="Home-workshop-paragraph">
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been{" "}
+          </p>
+          <button className="Home-workshop-button">Read More</button>
+        </div>
+        <div className="Home-Workshop-news">
+          <img
+            src="assets/certification.jpg"
+            alt=""
+            className="Home-workshop-img"
+          ></img>
+          <h3>Plumbing Certification</h3>
+          <h5>282 words 1.6 min read</h5>
+          <div>
+            <hr className="Home-Workshop-hr"></hr>
+          </div>
+          <p className="Home-workshop-paragraph">
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been{" "}
+          </p>
+          <button className="Home-workshop-button">Read More</button>
+        </div>
+      </div>
+
+    <div className="Home-feedback-container">
+      <div className="Home-feedback-one">
+      <h6>GET IN TOUCH TODAY</h6>
+      <h1>Receive Your Free Quote</h1>
+      <div>
+            <hr className="Home-Workshop-hr"></hr>
+      </div>
+     <div className="Home-feedback-name">
+        <div className="Home-feedback-firstname">
+        <label>First Name :</label>
+        <div>
+        <input className="Home-feedback-input"></input>
+        </div>
+        </div>
+        <div className="Home-feedback-firstname">
+        <label>Surname :</label>
+        <input className="Home-feedback-input"></input>
+        </div>
+     </div>
+
+     <div className="Home-feedback-name">
+        <div className="Home-feedback-firstname">
+        <label>Your Email :</label>
+        <input className="Home-feedback-input"></input>
+        </div>
+
+        <div className="Home-feedback-firstname">
+        <label>Telephone :</label>
+        <input className="Home-feedback-input"></input>
+        </div>
+     </div>
+
+          <div className="Home-feedback-message">
+              <label>Your Message:</label>
+              <div>
+                <input className="Home-feedback-input-message"></input>
+              </div>
+            </div>
+      
+            <div className="Home-feedback-button-container">
+                <button className="Home-feedback-button">Sumbit Your Quote Request</button>
+            </div>
+        
+      </div>
+     
+
+
+      <div className="Home-feedback-two">
+        <div className="Home-feedback-box">
+          <img src="assets/feedbackimg.jpeg" className="Home-feedback-img" alt=""></img>
+          <h2>"Outstanding Work”</h2>
+          <div className="Home-feedback-star">
+          <FaStar />
+          <FaStar />
+          <FaStar />
+          <FaStar />
+          <FaStar />
+          </div>
+          <p className="Home-feedback-paragraph">Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+           Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+            when an unknown printer took a galley of type and scrambled it to make a type 
+            specimen book. It has survived not only five centuries,
+           but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+           <h3>Shona Jones</h3>
+        </div>
+      </div>
+    </div>
+          
     </div>
   );
 }
